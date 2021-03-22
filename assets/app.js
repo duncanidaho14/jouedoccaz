1619abd9867f089372900b90a072d3c3b910fcd5
duncanidaho14 <elhadibeddarem@gmail.com>
2021-03-22 16:36:18 +0100

    install bootstrap

--- assets/app.js
+++ assets/app.js
@@ -5,10 +5,14 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 import $ from 'jquery';
+global.$ = global.jQuery = $;
+
+import '@popperjs/core';
 
 
 // any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap';
+
 import './styles/app.scss';
 
 // start the Stimulus application
-import './bootstrap';
